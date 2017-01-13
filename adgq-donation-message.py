04e0809903563df6889b67f0840008b6e5aab19a
John Koelndorfer <jkoelndorfer@gmail.com>
2017-01-13 15:55:55 -0600

    More work.

--- adgq-donation-message.py
+++ adgq-donation-message.py
@@ -22,6 +22,15 @@ class StoryTeller(object):
     def young_age(self):
         return random.choice(range(4, 14))
 
+    def final_fantasy_number(self, old_only):
+        min_number = 1
+        max_number = 16
+        if old_only:
+            max_number = 11
+        return random.choice(
+            list(filter(lambda i: i not in (11, 14), range(min_number, max_number)))
+        )
+
     def fmt_dict(self):
         attrs = (getattr(self, a) for a in dir(self))
         story_details = filter(lambda attr: getattr(attr, "is_story_detail", False) is True, attrs)
@@ -36,15 +45,21 @@ class StoryTeller(object):
     def loss_age(self):
         return self.young_age()
 
+    def old_game(self):
+        return random.choice((
+            "Super Metroid", "Contra", "Super Mario Bros", "Yoshi's Island", "Mega Man",
+            "Link to the Past", "Ocarina of Time", "Final Fantasy {final_fantasy_number}"
+        )).format(final_fantasy_number=self.final_fantasy_number(old_only=True))
+
     @story_detail
     def game(self):
-        return random.choice((
-            "Super Metroid", "Contra 3", "Super Mario Bros", "Yoshi's Island"
-        ))
+        return self.old_game()
 
     @story_detail
     def native_country(self):
-        return random.choice(("Germany", "Sweden"))
+        return random.choice((
+            "Belgium", "France", "Germany", "The Netherlands", "Norway", "Sweden"
+        ))
 
     @story_detail
     def extortion(self):
@@ -78,14 +93,17 @@ class StoryTeller(object):
             "the {game} blindfolded run", "saving the animals",
             "killing the animals", "the {game} glitch exhibition",
             "{game} two players, one controller",
-            "runner's choice", "reader's choice", "commentator's choice"
+            "runner's choice", "reader's choice", "commentator's choice",
+            "{game} 100% completion"
         )).format(game=self.game())
 
     @story_detail
     def close_person(self):
         return random.choice((
-            Person("mom", "she"), Person("dad", "he"),
-            Person("grandma", "she"), Person("grandpa", "he")
+            Woman("mom"), Man("dad"),
+            Woman("aunt"), Man("uncle"),
+            Woman("grandma"), Man("grandpa"),
+            Woman("best friend"), Man("best friend")
         ))
 
     @story_detail
@@ -99,7 +117,9 @@ class StoryTeller(object):
     @story_detail
     def type_of_cancer(self):
         return random.choice((
-            "breast cancer", "lung cancer", "leukemia", "melanoma"
+            "bone cancer", "boneitis (it was {close_person_pronoun} only regret)", "brain cancer", "breast cancer",
+            "cancer", "lung cancer", "leukemia", "melanoma", "pancreatic cancer", "prostate cancer",
+            "testicular cancer", "skin cancer"
         ))
 
 
@@ -109,18 +129,33 @@ class Person(object):
         self.gender_pronoun = gender_pronoun
 
 
+class Man(Person):
+    def __init__(self, relation):
+        super().__init__(relation, "he")
+
+
+class Woman(Person):
+    def __init__(self, relation):
+        super().__init__(relation, "she")
+
+
 class MessageBuilder(object):
-    def __init__(self):
+    def __init__(self, story_teller=None):
         self.greetings_from_country = False
         self.sob_story = False
         self.extortion = False
         self.allocated_donation_incentive = False
-        self.story_teller = StoryTeller()
+        if story_teller is None:
+            story_teller = StoryTeller()
+        self.story_teller = story_teller
 
     def m10_greeting(self):
         country_greet = "Greetings from {native_country}!"
         greet = random.choice((
-            "Hey guys!", "Hey all!", "Hey AGDQ!", country_greet,
+            "Hi guys!", "Hi all!", "Hi AGDQ!",
+            "Hello guys!", "Hello all!",
+            "Hey guys!", "Hey all!", "Hey AGDQ!",
+            "Greetings!", country_greet,
         ))
         if greet is country_greet:
             self.greetings_from_country = True
@@ -128,7 +163,9 @@ class MessageBuilder(object):
 
     def m15_subgreeting(self):
         return random.choice((
-            "Long time watcher, first time donator.", None
+            "Long time watcher, first time donator.",
+            "I've watched AGDQ for years, but this is my first time donating.",
+            None
         ))
 
     def m20_body(self):
@@ -200,6 +237,7 @@ class MessageBuilder(object):
 
         if not self.extortion:
             sob_closings.append("I wish I could donate more.")
+            sob_closings.append("I'd give more if I could.")
 
         if not self.greetings_from_country:
             closings.append(greetings_from_country)
@@ -220,13 +258,11 @@ class MessageBuilder(object):
             return " " + keep_it_up
         return ""
 
-    @classmethod
-    def generate(cls):
-        builder = cls()
-        methods = sorted((getattr(builder, m) for m in dir(builder) if m.startswith("m")), key=lambda m: m.__name__)
+    def generate(self):
+        methods = sorted((getattr(self, m) for m in dir(self) if m.startswith("m")), key=lambda m: m.__name__)
         msg_template = " ".join(filter(lambda x: x is not None, (m() for m in methods)))
-        return msg_template.format(**builder.story_teller.fmt_dict(), keep_it_up=builder.keep_it_up())
+        return msg_template.format(**self.story_teller.fmt_dict(), keep_it_up=self.keep_it_up())
 
 
 if __name__ == "__main__":
-    print(MessageBuilder.generate())
+    print(MessageBuilder().generate())
